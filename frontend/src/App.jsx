e5e15b31b141b70e81499f7e37cb6c769f954799
Mark <mark.irungu@student.moringaschool.com>
2026-04-09 11:30:00 +0300

    feat: add protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import VerifyEmailPage from './pages/VerifyEmailPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
@@ -10,6 +11,16 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/verify/:token" element={<VerifyEmailPage />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <div>Home Page - Coming Soon</div>
+          </ProtectedRoute>
+        } />
+        <Route path="/admin" element={
+          <ProtectedRoute adminOnly={true}>
+            <div>Admin Dashboard - Coming Soon</div>
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   )
