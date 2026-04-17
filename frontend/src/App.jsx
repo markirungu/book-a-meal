cbe400e3da2d865b0ce75d8d9238f82ef07f3e91
Mark <mark.irungu@student.moringaschool.com>
2026-04-17 19:50:16 +0300

    merge: resolve App.jsx conflicts, combine all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,56 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import VerifyEmailPage from './pages/VerifyEmailPage'
-import NotificationsPage from './pages/NotificationsPage'  // ✅ ADD THIS LINE
+import SignupPage from './pages/SignupPage'
+import MenuPage from './pages/MenuPage'
+import OrdersPage from './pages/OrdersPage'
+import AdminMealsPage from './pages/AdminMealsPage'
+import NotificationsPage from './pages/NotificationsPage'
 import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+        {/* Public pages - anyone can visit */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/verify/:token" element={<VerifyEmailPage />} />
-        {/* ✅ ADD THIS ROUTE */}
+
+        {/* Customer pages - must be logged in */}
+        <Route path="/menu" element={
+          <ProtectedRoute>
+            <MenuPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/orders" element={
+          <ProtectedRoute>
+            <OrdersPage />
+          </ProtectedRoute>
+        } />
         <Route path="/notifications" element={
           <ProtectedRoute>
             <NotificationsPage />
           </ProtectedRoute>
         } />
+
+        {/* Admin pages - must be logged in AND be an admin */}
+        <Route path="/admin/meals" element={
+          <ProtectedRoute adminOnly={true}>
+            <AdminMealsPage />
+          </ProtectedRoute>
+        } />
+
+        {/* Default redirect */}
         <Route path="/" element={
           <ProtectedRoute>
-            <div>Home Page - Coming Soon</div>
-          </ProtectedRoute>
-        } />
-        <Route path="/admin" element={
-          <ProtectedRoute adminOnly={true}>
-            <div>Admin Dashboard - Coming Soon</div>
-          </ProtectedRoute>
-        } />
-        <Route path="/admin/menu" element={
-          <ProtectedRoute adminOnly={true}>
-            <div>Menu Management - Coming Soon</div>
+            <MenuPage />
           </ProtectedRoute>
         } />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
