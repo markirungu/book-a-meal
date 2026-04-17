955cdf8475bab01f75382ba7db0f17ca7998eb97
Mark <mark.irungu@student.moringaschool.com>
2026-04-17 20:01:53 +0300

    fix: replace faulty Imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import VerifyEmailPage from './pages/VerifyEmailPage'
-import SignupPage from './pages/SignupPage'
-import MenuPage from './pages/MenuPage'
-import OrdersPage from './pages/OrdersPage'
-import AdminMealsPage from './pages/AdminMealsPage'
 import NotificationsPage from './pages/NotificationsPage'
 import ProtectedRoute from './components/ProtectedRoute'
 
@@ -13,40 +9,22 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public pages - anyone can visit */}
+        {/* Public pages */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/signup" element={<SignupPage />} />
         <Route path="/verify/:token" element={<VerifyEmailPage />} />
 
-        {/* Customer pages - must be logged in */}
-        <Route path="/menu" element={
-          <ProtectedRoute>
-            <MenuPage />
-          </ProtectedRoute>
-        } />
-        <Route path="/orders" element={
-          <ProtectedRoute>
-            <OrdersPage />
-          </ProtectedRoute>
-        } />
+        {/* Customer pages */}
         <Route path="/notifications" element={
           <ProtectedRoute>
             <NotificationsPage />
           </ProtectedRoute>
         } />
 
-        {/* Admin pages - must be logged in AND be an admin */}
-        <Route path="/admin/meals" element={
-          <ProtectedRoute adminOnly={true}>
-            <AdminMealsPage />
-          </ProtectedRoute>
-        } />
-
-        {/* Default redirect */}
+        {/* Default redirect to notifications */}
         <Route path="/" element={
           <ProtectedRoute>
-            <MenuPage />
+            <NotificationsPage />
           </ProtectedRoute>
         } />
       </Routes>
